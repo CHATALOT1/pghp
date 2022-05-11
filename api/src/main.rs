2611d5f7060f45e20bee33e9e55a6a1b74b67df5
Sylvia Mills <me@chatalot.xyz>
2022-05-11 18:47:36 +0100

    Small `use` statement refactor in API main file

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,12 +3,10 @@ mod settings;
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
-use settings::get_instance_settings;
-
 #[tokio::main]
 async fn main() {
-    let cfg = get_instance_settings();
     let app = Router::new().route("/*_", get(|| async { "More coming soon!" }));
+    let cfg = settings::get_instance_settings();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], cfg.port));
     axum::Server::bind(&addr)
