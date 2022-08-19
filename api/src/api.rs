136ae27cf61430796082f3c4a0cd7fc56760b2bd
Sylvia Mills <me@chatalot.xyz>
2022-08-19 20:34:55 +0100

    Remove previous setup

--- api/src/api.rs
+++
@@ -1,9 +0,0 @@
-use axum::{routing::get, Router};
-
-pub fn main() -> Router {
-    Router::new().route("/", get(temp_api_placeholder))
-}
-
-async fn temp_api_placeholder() -> &'static str {
-    "API coming soon! :D"
-}
